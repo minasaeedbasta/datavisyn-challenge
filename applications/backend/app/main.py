b4dc48269e07ef14771341ae609d08ed1209be03
minasaeedbasta <mina.saeed.basta@gmail.com>
2025-05-23 20:17:11 +0300

    fix(applications): update backend cors and endpoints

--- applications/backend/app/main.py
+++ applications/backend/app/main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["https://datavisyn.online"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI"}
-
-@app.get("/products")
-def read_products():
-    return {"products": ["Product A", "Product B"]}
 
 @app.get("/home")
 def read_home():
