de75a1d378f01f1ba13804272ce7e44829d9b41d
minasaeedbasta <mina.saeed.basta@gmail.com>
2025-05-24 21:06:40 +0300

    feat(application): simplify backend to /hello endpoint

--- applications/backend/app/main.py
+++ applications/backend/app/main.py
@@ -1,22 +1,7 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = ["https://datavisyn.online"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.get("/")
+@app.get("/hello")
 def read_root():
     return {"message": "Hello from FastAPI"}
-
-@app.get("/home")
-def read_home():
-    return {"message": "Welcome Home"}
